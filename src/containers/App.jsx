944bb5ced0293b3f78ee9c71b35a63ec9ef5a582
Žan Rode <zanr.comedy@gmail.com>
2022-01-29 17:20:41 +0100

    Basic setup

---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import Reactfrom "react";
+import Slide from '../components/Slide'
+import "tachyons";
+import "./App.css";
+
+const App = () => {
+  return <main>
+      <div className="container">
+        <Slide />
+      </div>
+  </main>;
+};
+
+export default App;
